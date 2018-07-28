0ace82a9b21362892eb8de2c7f9d04b9c763592d
jaiveleza <40079357+jaiveleza@users.noreply.github.com>
2018-07-28 12:49:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- Activity No.1  	Jaiveleza B. Andales     MIT         July 14, 2018
+-->
+
+
+<html>
+<head>
+<title>Activity No.1</title>
+<h1>Jaiveleza B. Andales</h1>
+<h2>Personal Information 
+    <img src="MyPix.jpg" style="width:80px;height:80px;"></h2>
+<h3>Ched Scholar - Batch 2017</h3>
+<h4>Faculty-UC-Banilad</h4>
+<h5>Contact Number: 0949-137-6940</h5>
+<h6>jaiveleza@gmail.com</h6>
+<h2>Additional Details</h2>
+<ul>
+<li><p> Age		: 38 years old</p></li>
+<li><p> Birthday	: May 20, 1980</p></li>
+<li><p> Gender	: Female</p></li>
+<li><p> Address	: Phase 1, Blk 1, Lot 9, Colorado Homes, Brgy. Cotcot, Liloan, Cebu 6002 </p></li>
+<li><p> Civil Status: Married</p></li>
+</ul>
+<br>
+<h2>Goal:</h2>
+<p>
+ My goal is to <b>finish my masteral studies</b>, <i>apply for ranking in the institution where I am
+ currently working</i> and be able to save an amount from my salary for future purposes.
+</p>
+<h2>Objectives</h2>
+<ol>
+<li><p>  Pass the comprehensive exam this <u>October 2018</u> </p></li>
+<li><p>   Finish thesis proposal and thesis writing on <em>May 2019</em> </p></li>
+<li><p>   Present my system in an international conference </p></li>
+<li><p>  <strong>Graduate MIT in USJR this October 201</strong></p></li>
+</ol>
+
+</body>
+</html>
