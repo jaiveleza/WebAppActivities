7b98dc8a9a18c0ca6d0be3582db76a7a542e52c2
jaiveleza <40079357+jaiveleza@users.noreply.github.com>
2019-01-12 22:00:26 +0800

    Update connection.php

--- MedServ08/connection.php
+++ MedServ08/connection.php
@@ -1,9 +1,9 @@
 <?php
 // DATABASE CONNECTION STRING
-$ser_ver = "localhost";
-$user_name = "root";
-$pass_word = "rootadmin";
-$datab = "medserv";
+$ser_ver = "sql3.freemysqlhosting.net";
+$user_name = "sql3273664";
+$pass_word = "KQbSlbH91h";
+$datab = "sql3273664";
 
 $db = new mysqli($ser_ver, $user_name, $pass_word, $datab);
 if ($db->connect_error) {
