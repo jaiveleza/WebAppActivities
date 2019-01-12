a35a730354bb62a02bde0ca77f99288c2e49d51c
jaiveleza <40079357+jaiveleza@users.noreply.github.com>
2019-01-12 20:05:21 +0800

    Update server.php

--- registration/server.php
+++ registration/server.php
@@ -30,7 +30,7 @@
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
 			$password = md5($password_1);//encrypt the password before saving in the database
-			$query = "INSERT INTO users (username, email, password)
+			$query = "INSERT INTO us_ers (username, email, password)
 					  VALUES('$username', '$email', '$password')";
 			mysqli_query($db, $query);
 
@@ -57,7 +57,7 @@
 
 		if (count($errors) == 0) {
 			$password = md5($password);
-			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+			$query = "SELECT * FROM us_ers WHERE username='$username' AND password='$password'";
 			$results = mysqli_query($db, $query);
 
 			if (mysqli_num_rows($results) == 1) {
