5fb6428ebb3b935cfabebe86d2e83859eea5e3ec
jaiveleza <40079357+jaiveleza@users.noreply.github.com>
2019-01-13 11:40:34 +0800

    Update login.php

--- MedService/login.php
+++ MedService/login.php
@@ -9,7 +9,7 @@ $num_rows="";
 //Connect to database
 //$connection=mysql_connect('localhost', 'root', 'rootadmin');
 //$db=mysql_select_db('medserv', $connection);
-$db = mysqli_connect('sql3.freemysqlhosting.net', 'sql3273664', 'KQbSlbH91h', 'sql3273664');
+$db = mysqli_connect("sql3.freemysqlhosting.net", "sql3273664", "KQbSlbH91h", "sql3273664");
 
 if(isset($_POST['submit']))
 {
