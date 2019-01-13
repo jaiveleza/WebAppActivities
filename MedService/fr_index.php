0cfd71eb20ff406234d0a6297132098dc3bfe23a
jaiveleza <40079357+jaiveleza@users.noreply.github.com>
2019-01-13 13:13:52 +0800

    Update fr_index.php

--- MedService/fr_index.php
+++ MedService/fr_index.php
@@ -14,7 +14,7 @@ include_once('fr_functions.php');
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
     <title>MedServ</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 <link rel="stylesheet" href="content.css" type="text/css"/>
 
 <script src="jquery.min.js"></script> 
