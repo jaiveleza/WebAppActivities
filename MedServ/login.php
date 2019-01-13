b0c2db06fd3c5b48594cf63271d5a147c94faa77
jaiveleza <40079357+jaiveleza@users.noreply.github.com>
2019-01-13 12:02:56 +0800

    Update login.php

--- MedServ/login.php
+++ MedServ/login.php
@@ -57,7 +57,7 @@ if(isset($_POST['submit']))
           {
           $errorMessage = $errorMessage."Username or password is invalid!";
           }
-          mysql_close($connection);
+          mysqli_close($connection);
 
           }
 
