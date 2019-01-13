1900f34769422ace609655cde5c70eb1badbf041
jaiveleza <40079357+jaiveleza@users.noreply.github.com>
2019-01-13 22:59:22 +0800

    Update connection.php

--- calend5/connection.php
+++ calend5/connection.php
@@ -1,30 +1,27 @@
 <?php
 // DATABASE CONNECTION STRING
-$ser_ver = 'sql3.freemysqlhosting.net';
-$user_name = 'sql3273664';
-$pass_word = 'KQbSlbH91h';
-$datab = 'sql3273664';
+//$ser_ver = 'sql3.freemysqlhosting.net';
+//$user_name = 'sql3273664';
+//$pass_word = 'KQbSlbH91h';
+//$datab = 'sql3273664';
 
 //define('localhost');
 //define('root');
 //define('rootadmin');
 //define('event_calendar');
 
-//define('HOST', 'localhost');
-//define('USERNAME', 'root');
-//define('PASSWORD', 'rootadmin');
-//define('DATABASE_NAME', 'event_calendar');
+define('HOST', 'sql3.freemysqlhosting.net');
+define('USERNAME', 'sql3273664');
+define('PASSWORD', 'KQbSlbH91h');
+define('DATABASE_NAME', 'sql3273664');
 
 //Connect and select the database
-//$db = new mysqli(HOST, USERNAME, PASSWORD, DATABASE_NAME);
+$db = new mysqli(HOST, USERNAME, PASSWORD, DATABASE_NAME);
 
-$db = new mysqli($ser_ver, $user_name, $pass_word, $datab);
-
-
+//$db = new mysqli($ser_ver, $user_name, $pass_word, $datab);
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
-
 //$connection = mysql_connect($ser_ver, $user_name, $pass_word);
 //$db = mysql_select_db($datab, $connection);
 
